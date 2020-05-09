4e100a6fef9e6899857960c6a992b6227df17ec9
Ralph-Gordon Paul <development@rgpaul.com>
2020-05-09 17:52:49 +0200

    Try automatic building again
    No errors, but isn't creating all artifacts

--- build-boost-win10.ps1
+++ build-boost-win10.ps1
@@ -17,85 +17,116 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # ----------------------------------------------------------------------------------------------------------------------
 
+
+# ATTENTION: execute this script using "Developer Powershell for VS 2019"
+
 #=======================================================================================================================
 # settings
 
-$LIBRARY_VERSION = "1.68.0"
+$LIBRARY_VERSION = "1.73.0"
 
 #=======================================================================================================================
 # globals
 
-$LIBRARY_VERSION2 = $LIBRARY_VERSION.Replace(".","_")
+$LIBRARY_VERSION2 = $LIBRARY_VERSION.Replace(".", "_")
 $LIBRARY_TARBALL = "boost_${LIBRARY_VERSION2}.zip"
 $LIBRARY_DOWNLOAD_URI = "https://dl.bintray.com/boostorg/release/${LIBRARY_VERSION}/source/${LIBRARY_TARBALL}"
 $LIBRARY_TARBALL_PATH = "${PSScriptRoot}\${LIBRARY_TARBALL}"
 $LIBRARY_FOLDER = "${PSScriptRoot}\boost_${LIBRARY_VERSION2}"
 $LIBRARY_INSTALL_FOLDER = "${PSScriptRoot}\install"
 $LIBRARY_BUILD_FOLDER = "${PSScriptRoot}\build"
+$OLD_PATH = $env:Path
 
 #=======================================================================================================================
 
-function Download()
-{
+function Download() {
     # only download if not already present
-    if (!(test-path $LIBRARY_TARBALL_PATH))
-    {
+    if (!(test-path $LIBRARY_TARBALL_PATH)) {
         Write-Host "File ${LIBRARY_TARBALL} does not exist. Download now...`n`n"
 
         # set protocol to tls version 1.2
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
         Invoke-WebRequest -Uri $LIBRARY_DOWNLOAD_URI -OutFile $LIBRARY_TARBALL_PATH
     }
-    else
-    {
+    else {
         Write-Host "File ${LIBRARY_TARBALL} already existing"
     }
 }
 
 #=======================================================================================================================
 
-function Unpack()
-{
-    Write-Host "Unpacking zip file..."
-    Expand-Archive -Path $LIBRARY_TARBALL_PATH -DestinationPath $PSScriptRoot
+function Unpack() {
+
+    # only download if not already present
+    if (!(test-path $LIBRARY_FOLDER)) {
+        Write-Host "Unpacking zip file..."
+        Expand-Archive -Path $LIBRARY_TARBALL_PATH -DestinationPath $PSScriptRoot
+    }
+    else {
+        Write-Host "Not extracting: ${LIBRARY_FOLDER} already existing"
+    }
 }
 
 #=======================================================================================================================
 
-function Build($arch, $model)
-{
-    Set-Location "${LIBRARY_FOLDER}\tools\build"
-    $buildPath = "${LIBRARY_INSTALL_FOLDER}\${arch}_${model}"
+function Build($arch, $model, $variant) {
+    Push-Location "${LIBRARY_FOLDER}\tools\build"
+    $buildPath = "${LIBRARY_BUILD_FOLDER}_${arch}_${model}_${variant}"
     New-Item -ItemType Directory -Path "${buildPath}"
 
-    & "${LIBRARY_FOLDER}\tools\build\bootstrap.bat"
-    & .\b2 "--prefix=${buildPath}" toolset=msvc architecture=${arch} address-model=${model} link=static runtime-link=static threading=multi variant=debug install
-    $env:Path += ";${buildPath}\bin"
+    ".\${LIBRARY_FOLDER}\tools\build\bootstrap.bat"
+    .\b2 "--prefix=${buildPath}" toolset=msvc architecture=${arch} address-model=${model} link=static runtime-link=static threading=multi variant=${variant} install
 
-    Set-Location "${LIBRARY_FOLDER}"
-    & b2 "--build-dir=${buildPath}" toolset=msvc architecture=${arch} address-model=${model} link=static runtime-link=static threading=multi variant=debug stage
+    $env:Path = "${OLD_PATH};${buildPath}\bin"
 
-    Set-Location $PSScriptRoot
+    Push-Location "${LIBRARY_FOLDER}"
+    b2 "--build-dir=${buildPath}" toolset=msvc architecture=${arch} address-model=${model} link=static runtime-link=static threading=multi variant=${variant} stage
+
+    Pop-Location
+    Pop-Location
 }
 
 #=======================================================================================================================
 
-function Cleanup()
-{
-    Remove-Item -Recurse -Path ${LIBRARY_BUILD_FOLDER}, ${LIBRARY_FOLDER}, ${LIBRARY_INSTALL_FOLDER}
+function Cleanup() {
+    # Remove-Item -Recurse -Path ${LIBRARY_INSTALL_FOLDER}
+
+    if (!(test-path "${LIBRARY_BUILD_FOLDER}_x86_32_release")) {
+        Remove-Item -Recurse -Path "${LIBRARY_BUILD_FOLDER}_x86_32_release"
+    }
+    if (!(test-path "${LIBRARY_BUILD_FOLDER}_x86_32_debug")) {
+        Remove-Item -Recurse -Path "${LIBRARY_BUILD_FOLDER}_x86_32_debug"
+    }
+    if (!(test-path "${LIBRARY_BUILD_FOLDER}_x86_64_release")) {
+        Remove-Item -Recurse -Path "${LIBRARY_BUILD_FOLDER}_x86_64_release"
+    }
+    if (!(test-path "${LIBRARY_BUILD_FOLDER}_x86_64_debug")) {
+        Remove-Item -Recurse -Path "${LIBRARY_BUILD_FOLDER}_x86_64_debug"
+    }
+    if (!(test-path "${LIBRARY_BUILD_FOLDER}_arm_32_release")) {
+        Remove-Item -Recurse -Path "${LIBRARY_BUILD_FOLDER}_arm_32_release"
+    }
+    if (!(test-path "${LIBRARY_BUILD_FOLDER}_arm_32_debug")) {
+        Remove-Item -Recurse -Path "${LIBRARY_BUILD_FOLDER}_arm_32_debug"
+    }
+    if (!(test-path "${LIBRARY_BUILD_FOLDER}_arm_64_release")) {
+        Remove-Item -Recurse -Path "${LIBRARY_BUILD_FOLDER}_arm_64_release"
+    }
+    if (!(test-path "${LIBRARY_BUILD_FOLDER}_arm_64_debug")) {
+        Remove-Item -Recurse -Path "${LIBRARY_BUILD_FOLDER}_arm_64_debug"
+    }
 }
 
 #=======================================================================================================================
 
-function Archive()
-{
-    New-Item -ItemType Directory -Path ${LIBRARY_INSTALL_FOLDER}\lib
-    Move-Item -Path ${LIBRARY_INSTALL_FOLDER}\lib_x86 -Destination ${LIBRARY_INSTALL_FOLDER}\lib\x86
-    Move-Item -Path ${LIBRARY_INSTALL_FOLDER}\lib_x64 -Destination ${LIBRARY_INSTALL_FOLDER}\lib\x64
-    Move-Item -Path ${LIBRARY_INSTALL_FOLDER}\lib_arm -Destination ${LIBRARY_INSTALL_FOLDER}\lib\arm
+function Archive() {
+    New-Item -ItemType Directory -Path ${LIBRARY_INSTALL_FOLDER}\include
+
+    Copy-Item -Path ${LIBRARY_FOLDER}\stage\lib -Destination ${LIBRARY_INSTALL_FOLDER}\
+    Copy-Item -Path ${LIBRARY_FOLDER}\boost -Destination ${LIBRARY_INSTALL_FOLDER}\include\
 
     Compress-Archive -Force -Path ${LIBRARY_INSTALL_FOLDER}\include, ${LIBRARY_INSTALL_FOLDER}\lib `
-        -DestinationPath boost-${LIBRARY_SEMVER}-win10.zip
+        -DestinationPath boost-${LIBRARY_VERSION}-win10.zip
 }
 
 #=======================================================================================================================
@@ -104,25 +135,24 @@ Write-Host  "###################################################################
 Write-Host  "###                                  Boost                                   ###"
 Write-Host  "################################################################################`n"
 
-
-#Push-Location "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools"
-Push-Location "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools"
-cmd /c "VsDevCmd.bat&set" | 
-ForEach-Object { 
-    if ($_ -match "=") { 
-        $v = $_.split("="); set-item -force -path "ENV:\$($v[0])" -value "$($v[1])" 
-    } 
-} 
-Pop-Location 
-Write-Host "`nVisual Studio 2017 Command Prompt variables set." -ForegroundColor Yellow
+Push-Location ${PSScriptRoot}
 
 Download
 Unpack
-#Build Win32
-Build "x86" "64"
-#Build ARM
+Build "x86" "64" "release"
+Build "x86" "64" "debug"
+Build "x86" "32" "release"
+Build "x86" "32" "debug"
+Build "arm" "64" "release"
+Build "arm" "64" "debug"
+Build "arm" "32" "release"
+Build "arm" "32" "debug"
+
 #Archive
+
 #Cleanup
+
+Pop-Location
 
 Write-Host "`nPress any key to close...";
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
